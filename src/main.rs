a1815420d90f0a041dc734c84f30426fde70a727
Gaspard LEGENDRE <glegendr@e1r6p16.42.fr>
2018-06-25 23:00:46 +0200

    Feat: add args parser

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,14 @@ extern crate clap;
 use clap::{Arg, App, SubCommand};
 
 fn main() {
-    println!("Hello, world!");
+	let matches = App::new("Tycho")
+		.version("0.1")
+		.arg(Arg::with_name("initialize")
+			.short("i")
+			.long("init")
+			.value_name("NAME")
+			.takes_value(true)
+			.help("init a new C project"))
+		.get_matches();
+	println!("{:?}", matches);
 }
