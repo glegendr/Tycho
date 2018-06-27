60d395c18a244310f72b2659e97fdadc22791d85
Gaspard LEGENDRE <glegendr@e2r3p16.42.fr>
2018-06-27 22:55:28 +0200

    Feat: add flag deploy

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ extern crate clap;
 use clap::{Arg, App, SubCommand};
 use std::process::Command;
 use std::env;
+use std::path::Path;
+use std::fs::ReadDir;
 
 fn main() {
 	if env::var("TYCHO_PATH").is_err() {
 		println!("TYCHO_PATH unset");
 		std::process::exit(1);
 	}
-		
+
 	let matches = App::new("Tycho")
 		.version("0.1")
 		.arg(Arg::with_name("initialize")
@@ -17,9 +19,24 @@ fn main() {
 				.value_name("NAME")
 				.takes_value(true)
 				.help("init a new C project"))
+		.arg(Arg::with_name("update")
+				.short("u")
+				.long("update")
+				.takes_value(false)
+				.help("update the Makefile"))
+		.arg(Arg::with_name("deploy pod")
+				.short("d")
+				.long("deploy-pod")
+				.takes_value(true)
+				.value_name("POD_URL")
+				.help("deploy a pod in the solution"))
 		.get_matches();
 	if let Some(init_name) =  matches.value_of("initialize") {
 		init_project(init_name);
+	} else if matches.is_present("update") {
+		unimplemented!("it's build time !");
+	} else if let Some(pod) =  matches.value_of("deploy pod") {
+		deploy_pod(pod);
 	}
 }
 
@@ -47,6 +64,36 @@ fn init_project(init_name: &str) {
 			.spawn();
 	} else {
 		println!("failed to create {} directory", init_name);
+		std::process::exit(1);
+	}
+}
+
+fn get_pod_name(url: &str) -> &str {
+	url.split("/").last().unwrap().split(".").next().unwrap()
+}
+
+fn deploy_pod(pod: &str) {
+	let _ = Command::new("mkdir")
+		.arg("-p")
+		.arg("pods")
+		.status();
+	let gcl_status = Command::new("git")
+		.arg("clone")
+		.arg(pod)
+		.status()
+		.expect("");
+	if gcl_status.success() {
+		let name = get_pod_name(pod);
+		let _ = Command::new("mv")
+			.arg(name)
+			.arg("pods/")
+			.spawn();
+		let _ = Command::new("rm")
+			.arg("-rf")
+			.arg(format!("pods/{}/.git", name))
+			.spawn();
+	} else {
+		println!("failed to deploy your pod");
 		std::process::exit(1);
 	}
 }
