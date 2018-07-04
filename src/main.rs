1dfea2d941f8e0e52101ee32a86cf9e3b1c73ab2
Gaspard LEGENDRE <glegendr@e1r7p18.42.fr>
2018-07-04 04:21:14 +0200

    initialize with .git

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ extern crate clap;
 use clap::{Arg, App, SubCommand};
 use std::process::Command;
 use std::env;
-use std::path::Path;
-use std::fs::ReadDir;
 
 fn main() {
 	if env::var("TYCHO_PATH").is_err() {
@@ -30,22 +28,44 @@ fn main() {
 				.takes_value(true)
 				.value_name("POD_URL")
 				.help("deploy a pod in the solution"))
+		.arg(Arg::with_name("create vessel")
+				.short("g")
+				.long("git")
+				.takes_value(false)
+				.help("create a repo git for your project (it has to be use with -i)"))
 		.get_matches();
-	if let Some(init_name) =  matches.value_of("initialize") {
-		init_project(init_name);
-	} else if matches.is_present("update") {
+	if let Some(init_name) = matches.value_of("initialize") {
+		if matches.is_present("create vessel") {
+			init_project(init_name, true);
+		} else {
+			init_project(init_name, false);
+		}
+	}
+	if matches.is_present("update") {
 		unimplemented!("it's build time !");
-	} else if let Some(pod) =  matches.value_of("deploy pod") {
+	}
+	if let Some(pod) =  matches.value_of("deploy pod") {
 		deploy_pod(pod);
 	}
 }
 
-fn init_project(init_name: &str) {
-	let status = Command::new("mkdir")
-		.arg(init_name)
-		.status()
-		.expect("");
-	if status.success() {
+fn init_project(init_name: &str, git: bool) {
+	let tmp;
+	if git == true {
+		let status = Command::new("git")
+			.arg("init")
+			.arg(init_name)
+			.status()
+			.expect("");
+			tmp = status;
+	} else {
+		let status = Command::new("mkdir")
+			.arg(init_name)
+			.status()
+			.expect("");
+			tmp = status;
+	}
+	if tmp.success() {
 		let _ = Command::new("mkdir")
 			.arg(format!("{}/src", init_name))
 			.arg(format!("{}/inc", init_name))
@@ -62,6 +82,12 @@ fn init_project(init_name: &str) {
 			.arg(format!("{}/template/inc.h", env::var("TYCHO_PATH").unwrap()))
 			.arg(format!("{0}/inc/{0}.h", init_name))
 			.spawn();
+		if git == true {
+			let _ = Command::new("cp")
+				.arg(format!("{}/template/.gitignore", env::var("TYCHO_PATH").unwrap()))
+				.arg(format!("{}/.gitignore", init_name))
+				.spawn();
+		}
 	} else {
 		println!("failed to create {} directory", init_name);
 		std::process::exit(1);
